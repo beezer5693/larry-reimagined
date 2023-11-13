01f5aff55db618e10d53eb96c3d29dcc5d9152e2
Brandon Bryan <bshwag@gmail.com>
2023-11-13 12:15:43 -0500

    Added animation to mobile menu

--- src/components/shared/navbar/MobileMenu.tsx
+++ src/components/shared/navbar/MobileMenu.tsx
@@ -1,7 +1,9 @@
+"use client";
+
 import { Button } from "@/components/ui/Button";
-import { cn } from "@/lib/utils";
-import { ChevronRight, X } from "lucide-react";
 import { NAV_LINKS } from "@/constants";
+import { cn } from "@/lib/utils";
+import { AnimatePresence, motion } from "framer-motion";
 import Link from "next/link";
 
 type Props = {
@@ -10,36 +12,104 @@ type Props = {
 };
 
 export default function MobileMenu({ isMenuOpen, closeMenu }: Props) {
+  const menuVariants = {
+    initial: {
+      scaleY: 0,
+    },
+    animate: {
+      scaleY: 1,
+      transition: {
+        duration: 0.5,
+        ease: [0.12, 0, 0.39, 0],
+      },
+    },
+    exit: {
+      scaleY: 0,
+      transition: {
+        delay: 0.5,
+        duration: 0.5,
+        ease: [0.22, 1, 0.36, 1],
+      },
+    },
+  };
+
+  const mobileMenuVariants = {
+    initial: {
+      y: "30vh",
+      transition: {
+        duration: 0.5,
+        ease: [0.37, 0, 0.63, 1],
+      },
+    },
+    open: {
+      y: 0,
+      transition: {
+        duration: 0.7,
+        ease: [0, 0.55, 0.45, 1],
+      },
+    },
+  };
+
+  const containerVariants = {
+    initial: {
+      transition: {
+        staggerChildren: 0.09,
+        staggerDirection: -1,
+      },
+    },
+    open: {
+      transition: {
+        delayChildren: 0.3,
+        staggerChildren: 0.09,
+        staggerDirection: 1,
+      },
+    },
+  };
   return (
-    <div
-      className={cn(
-        `fixed right-0 top-[3.55rem] z-[100] flex h-screen w-full flex-col bg-white transition-transform duration-500 ease-in-out lg:hidden ${
-          isMenuOpen ? "translate-x-0" : "translate-x-full"
-        }`,
-      )}
-    >
-      <div className="space-y-10">
-        <div className="pl-10">
-          {NAV_LINKS.map((tab) => (
-            <Link
-              onClick={closeMenu}
-              key={tab.label}
-              href={tab.href}
-              className="w-full"
+    <>
+      <AnimatePresence>
+        {isMenuOpen && (
+          <motion.div
+            className={cn(
+              `fixed right-0 top-[3.55rem] z-[100] flex h-screen w-full origin-top flex-col items-center bg-white pt-20 lg:hidden`,
+            )}
+            variants={menuVariants}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+          >
+            <motion.div
+              variants={containerVariants}
+              initial="initial"
+              animate="open"
+              exit="initial"
             >
-              <div className="flex w-full items-center justify-between border-b border-gray-200 py-6 pr-5">
-                <div className="font-medium text-gray-950">{tab.label}</div>
-                <ChevronRight className="text-gray-950" />
-              </div>
-            </Link>
-          ))}
-        </div>
-        <div className="w-full px-10">
-          <Link onClick={closeMenu} href="/contact">
-            <Button className="h-14 w-full text-xl">Book Larry</Button>
-          </Link>
-        </div>
-      </div>
-    </div>
+              {NAV_LINKS.map((tab, i) => (
+                <div key={tab.label} className="overflow-hidden">
+                  <motion.div variants={mobileMenuVariants} className="mb-5">
+                    <Link
+                      onClick={closeMenu}
+                      href={tab.href}
+                      className="w-full"
+                    >
+                      <div className="flex w-full flex-col items-center justify-center">
+                        <div className="text-5xl font-medium text-gray-950">
+                          {tab.label}
+                        </div>
+                      </div>
+                    </Link>
+                  </motion.div>
+                </div>
+              ))}
+            </motion.div>
+            <div className="mt-8 w-full max-w-[400px]">
+              <Link onClick={closeMenu} href="/contact">
+                <Button className="h-14 w-full text-xl">Book Larry</Button>
+              </Link>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </>
   );
 }
