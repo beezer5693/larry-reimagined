80c1d29d17d810453d65bc06fda4739c592b6352
Brandon Bryan <bshwag@gmail.com>
2024-01-31 07:39:42 -0500

    Updated quote on landing page

--- src/app/(routes)/(home)/_components/thoughts/ThoughtSection.tsx
+++ src/app/(routes)/(home)/_components/thoughts/ThoughtSection.tsx
@@ -9,9 +9,7 @@ const ThoughtSection = () => {
         <div className="mx-auto max-w-[800px] text-center text-2xl text-white md:text-3xl">
           <AnimateElement duration={1}>
             <div className="font-medium">
-              {`"Life is all about results and everyone wants them quickly. My
-              talks offer the keys to achieving those remarkable results in life
-              much faster."`}
+              {`"Everyone wants great results delivered quickly and my talks offer the keys to getting those results much faster."`}
             </div>
           </AnimateElement>
           <AnimateElement duration={1} delay={0.2}>
