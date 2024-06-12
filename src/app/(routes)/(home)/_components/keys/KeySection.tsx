6fc59c6e89f0ac3367b960ed8debdb095623d8b4
Brandon Bryan <bshwag@gmail.com>
2024-06-12 13:34:56 -0400

    text changes

--- src/app/(routes)/(home)/_components/keys/KeySection.tsx
+++ src/app/(routes)/(home)/_components/keys/KeySection.tsx
@@ -9,16 +9,16 @@ const KeySection = () => {
         <div className="flex w-full flex-col justify-between gap-8 text-gray-950 lg:flex-row lg:items-center">
           <AnimateElement className="basis-3/5" y={50} duration={0.75}>
             <h2 className="balance-text text-center font-semibold tracking-tight text-gray-950 lg:mr-32 lg:text-left">
-              My 5 essential traits to be a top 1% producer
+              Essential traits to becoming a top 1% sales producer
             </h2>
           </AnimateElement>
           <AnimateElement className="flex-1" duration={0.8} delay={0.2}>
             <p className="sub-heading text-center font-medium text-gray-800 lg:text-left">
-              It took me some time to realize how my mindset influenced my life,
-              but applying my{" "}
-              <span className="font-semibold">{`"Five Keys to a New Mindset"`}</span>{" "}
-              can bring transformative change and elevate your personal and
-              professional life and performance.
+              To become a top 1% producer, you need these 10 traits operating at
+              their highest level. This requires not only knowing what actions
+              to take but also adopting the right mindset. I'll guide you
+              through both aspects, showing you exactly what you need to do and
+              how you need to think to reach this elite level.
             </p>
           </AnimateElement>
         </div>
